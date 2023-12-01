9860a5ef587bf2ba0c8fb457970aaedf043aece5
鄭哲丞 <a0968518713@gmail.com>
2023-12-01 20:14:38 +0800

    管理員樣板設計

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +22,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\IndexController::class, 'index'])->name('home');
 
+
 //Route::get('/', [App\Livewire\Shopping\Index::class,'render'])->name('livewire.shopping.index');
 //登入
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminHomeController::class, 'index'])->name("home.index");
+});
+
+
 Auth::routes();
 
 

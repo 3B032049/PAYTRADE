7f48e3508d518e96bf95e6bc75bd2729948c20f6
劉秉樺 <alen920617@gmail.com>
2023-12-22 09:30:49 +0800

    建立order&order_detail的關係

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'seller_id',
+        'status',
+        'date',
+        'score',
+        'comment',
+        'pay',
+        'price',
+        'receiver',
+        'receiver_phone',
+        'receiver_address',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderdetail()
+    {
+        return $this->hasMany(orderdetail::class);
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(seller::class);
+    }
+
 }

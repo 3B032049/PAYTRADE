dbfc871753da31e5463f2a152cdd814d36959d0e
黃文輝 <antory040512@gmail.com>
2023-12-29 10:47:33 +0800

    修改賣場路由

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::get('/', [App\Http\Controllers\IndexController::class, 'index']);
 Route::get('/home', [App\Http\Controllers\IndexController::class, 'index'])->name('home');
 Route::get('products/search', [App\Http\Controllers\ProductController::class, 'search'])->name('products.search');
 Route::get('products/{product}', [App\Http\Controllers\ProductController::class, 'show'])->name('products.show');
-Route::get('products/{product}/{seller_id}', [App\Http\Controllers\ProductController::class, 'shopindex'])->name('shopindex');
+Route::get('products/product/{seller_id}', [App\Http\Controllers\ProductController::class, 'shopindex'])->name('shopindex');
 
 
 Route::get('products/by_category/{category_id}', [App\Http\Controllers\ProductController::class, 'by_category'])->name('products.by_category');

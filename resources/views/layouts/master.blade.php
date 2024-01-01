ac47f7c5a99858facf9c2ab457420ebb640d1b4f
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-01 18:27:59 +0800

    背景調整

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,11 +4,9 @@
         <meta charset="utf-8">
         <meta viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title')</title>
-{{--        <link rel="stylesheet" href="{{asset('css/styles.css')}}">--}}
-{{--        <link rel="stylesheet" href="{{asset('css/style.css')}}">--}}
-
         <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         <link href="{{asset('css/homepage-styles.css')}}" rel="stylesheet" />
 
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -17,26 +15,13 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>賣家賣場</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/shopstyles.css" rel="stylesheet" />
-
     </head>
     <body>
         @include('layouts.partials.navigation')
         <section id="location">
             @yield('content')
         </section>
-
-{{--        <!-- Bootstrap core JS-->--}}
-{{--        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>--}}
-{{--        <!-- Core theme JS-->--}}
-{{--        <script src="{{asset('js/scripts.js')}}"></script>--}}
-
         @include('layouts.partials.footer')
-{{--        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>--}}
-        <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
     </body>
 <html>
--- resources/views/products/index/layouts/master.blade.php
+++ resources/views/products/index/layouts/master.blade.php
@@ -4,15 +4,17 @@
         <meta charset="utf-8">
         <meta viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title')</title>
-{{--        <link rel="stylesheet" href="{{asset('css/styles.css')}}">--}}
-{{--        <link rel="stylesheet" href="{{asset('css/style.css')}}">--}}
-
         <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         <link href="{{asset('css/homepage-styles.css')}}" rel="stylesheet" />
 
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>賣家賣場</title>
     </head>
     <body>
         @include('layouts.partials.navigation')
@@ -20,5 +22,6 @@
             @yield('content')
         </section>
         @include('layouts.partials.footer')
+        <script src="js/scripts.js"></script>
     </body>
 <html>

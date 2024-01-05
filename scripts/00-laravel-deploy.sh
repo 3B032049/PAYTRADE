97851e86e4e765a42920ee869572c814cdf7e528
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-05 10:07:16 +0800

    執行腳本調整

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,13 +3,6 @@ composer self-update --2
 composer global require hirak/prestissimo
 composer install --working-dir=/var/www/html
 
-# 安裝 Node.js 和 npm
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# 設定 npm 的 PATH
-export PATH="$PATH:/path/to/npm"
-
 echo "Caching config..."
 php artisan config:cache
 
@@ -19,6 +12,3 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-echo "Running npm install and npm run build..."
-npm install
-npm run build

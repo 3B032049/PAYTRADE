02f3490504e6af585132ca7e34d4fc220a952042
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-11 20:17:48 +0800

    Model調整

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Seller extends Model
 {
@@ -22,5 +23,10 @@ class Seller extends Model
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
